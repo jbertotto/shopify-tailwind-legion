4d6b1dee65505a2bfb8a4f766867a706977a7b3b
galax1y <lucasvw.eng@gmail.com>
2023-11-20 01:29:21 -0300

    chore: Product card & icon-arrow customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,9 +43,17 @@ module.exports = {
       width: {
         icon: '3.2rem',
       },
-    },
-    color: {
-      black: '#000000',
+      colors: {
+        background: 'rgb(var(--color-background))',
+        foreground: 'rgb(var(--color-foreground))',
+        background_gradient: 'rgb(var(--gradient-background))',
+        text_color: 'rgb(var(--color-foreground))',
+        button: 'rgb(var(--color-button))',
+        button_label: 'rgb(var( --color-button-text))',
+        secondary_button_label: 'rgb(var(--color-secondary-button-text))',
+        accent: 'rgb(var(--color-accent))',
+        shadow: 'rgb(var(--color-shadow))',
+      },
     },
   },
   plugins: [],
